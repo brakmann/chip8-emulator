19ddff45734c4cd9acf567b00fee4b870b257b8c
brakmann <evgeniybrakmann@gmail.com>
2026-05-14 13:09:15 +0300

    Implement chip8 data structure

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
-#include <stdio.h> // Подключение стандартной библиотеки ввода-вывода
+#include <stdio.h>
+#include <stdint.h>
+
+typedef struct {
+        uint8_t memory[4096];
+        uint16_t stack[16];
+        uint8_t V[16];
+        uint16_t I;
+        uint16_t PC;
+        uint8_t SP;
+        uint8_t delay_timer;
+        uint8_t sound_timer;
+        uint8_t screen [32][64];
+        uint8_t keypad[16];
+    } Chip8;
 
 int main() {
-    // printf() выводит текст на экран
-    printf("Hello, World!\n"); 
-    
-    return 0; // Успешное завершение программы
+    return 0; 
 }
